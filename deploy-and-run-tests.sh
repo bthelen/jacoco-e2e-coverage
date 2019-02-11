1853901fcaab43b7d2cb5c4a8c0542137e914ecf
Bruce E. Thelen <bthelen@pivotal.io>
2019-02-11 09:52:12 -0600

    Trying to use the file mode for jacoco instead of the tcpclient mod

--- deploy-and-run-tests.sh
+++ deploy-and-run-tests.sh
@@ -9,27 +9,10 @@ set -ex
 # 4.  You have "expect" installed where this will run
 # 5.  Run this script from the directory where bthelen/jacoco-e2e-coverage is checked out
 
-# build and push --no-start jacoco tcp server
-pushd ../jacoco-tcpserver
-mvn clean install
-cf push -f manifest.yml --no-start
-
-# create the cups for the jacoco tcpserver
-cf cups my-jacoco-service -p '{"address":"jacoco-tcpserver.apps.internal","includes":"*", "port":"6300"}'
-
-# build and deploy --no-start client
-popd
+# build and deploy app we are going to use
 mvn clean install -DskipTests
-cf push -f manifest.yml --no-start
-
-# create network policy to let those two things speak
-cf add-network-policy jacoco-e2e-coverage --destination-app jacoco-tcpserver  --protocol tcp --port 6300
-
-# start jacoco tcp server
-cf start jacoco-tcpserver
-
-# start client
-cf start jacoco-e2e-coverage
+#cf cups my-jacoco-service -p '{"address":"jacoco-tcpserver.apps.internal","includes":"*", "port":"6300"}'
+cf push -f manifest.yml
 
 sleep 5
 
@@ -38,23 +21,12 @@ mvn test -Dapp.url="https://jacoco-e2e-coverage.apps.pcfone.io" -Dparameter=5
 
 sleep 5
 
-# stop and undeploy client
-cf stop jacoco-e2e-coverage
-
-sleep 5
-
 # copy file from jacoco tcp server -- but how to get password!!
 # scp -P 2222 -o User=cf:$(cf app jacoco-tcpserver --guid)/0  ssh.run.pcfone.io:/home/vcap/app/jacoco-server.exec .
-./get_jacoco_report.exp $(cf app jacoco-tcpserver --guid) $(cf ssh-code) ssh.run.pcfone.io
+#./get_jacoco_report.exp $(cf app jacoco-tcpserver --guid) $(cf ssh-code) ssh.run.pcfone.io
 
 # Run mvn to create the report from that file we just downloaded
-mvn antrun:run@generate-report -Dskip.int.tests.report=false
+#mvn antrun:run@generate-report -Dskip.int.tests.report=false
 
 # Undeploy the app we are testing
-cf delete jacoco-e2e-coverage -f
-
-# stop and undeploy jacoco tcp server
-cf delete jacoco-tcpserver -f
-
-# clean up unused UPS
-cf delete-service my-jacoco-service -f
+#cf delete jacoco-e2e-coverage -f
